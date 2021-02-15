ef5ff98dd1bb431f719f056456677c7c33b393d5
VasylKobil <vasylkobil@gmail.com>
2021-02-15 22:05:15 +0100

    Added new project to portfolio and improved mail form

--- js/scripts.js
+++ js/scripts.js
@@ -64,7 +64,7 @@
     ScrollOut({
         onShown: function(el) {
             // use the web animation API
-            el.animate([{ opacity: 0 }, { opacity: 1}],1500)
+            el.animate([{ opacity: 0 }, { opacity: 1 }],1000);
         },
         onHidden: function(el) {
             // hide the element initially
